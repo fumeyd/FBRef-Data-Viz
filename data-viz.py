7541b3a2915bcef6b34b15d0836e77667ea20a94
Delasie Fumey <delasie29@gmail.com>
2022-07-31 19:56:47 +0100

    added filtering logic and cleaned up raw data

--- data-viz.py
+++ data-viz.py
@@ -5,20 +5,40 @@ import matplotlib.cbook as cbook
 
 class dataViz: 
     def __init__(self, csvData):
-        self.data = csvData 
+        self.data = csvData
 
     def getData(self):
         with open(self.data) as csv_file:
-            data = csv_file.read()
+            data = csv.reader(csv_file)
         
         return(data)
 
     def getTitleColumns(self):
         with open(self.data) as csv_file:
             csv_reader = csv.reader(csv_file)
-            for i in range(0):
+            for i in range(1):
                 for row in csv_reader:
                     return row
+ 
+    def colNameMap(self):
+        dict = {};
+        columns = self.getTitleColumns()
+        idx = range(0, len(columns))
+        for i in range(len(idx)):
+            dict[columns[i]] = idx[i]
+
+        return dict
+
+    def getColumn(self, colName):
+        colNameMap = self.colNameMap()
+        colsData = []
+        with open(self.data) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            colIdx = colNameMap.get(colName)
+            for row in csv_reader:
+                colsData.append(row[colIdx])
+        
+        return(colsData)
 
     def getRow(self, teamName):  
         with open(self.data) as csv_file: 
@@ -27,24 +47,38 @@ class dataViz:
                 if teamName in row:
                     return(row)
 
-    def visualise(self):
-        csvFile = 'C:/Users/kwaks/OneDrive - Ashesi University/Personal Work Stuff/fbref-data-viz/fbref_squad_passing.csv'
-        cbookFile = cbook.open_file_cm(csvFile, mode='r')
+    def remAlphaBs(self, data):
+        for i in data:
+            try:
+                i = float(i)
+            except:
+                data.remove(i)
+
+        return(data)
+
+    def visualise(self, xList, yList):
+        # cbookFile = cbook.open_file_cm(self.data, mode='r')
+
+        fig, ax = plt.subplots(1, 1)
+        for i in range(len(xList)):
+            ax.plot(xList[i], yList[i])
         
-        # refinedData = (cbook.open_file_cm(csvFile, mode='r')['Prog']
-        # .view(np.recarray))
-        
-        fig, ax = plt.subplots()
+        plt.show
         
 
 def main():
     
     viz = dataViz('fbref_squad_passing.csv')
-    titles = viz.getTitleColumns()
     teamRow = viz.getRow('Arsenal')
-    viz.visualise();
 
-    print(titles)
+    map = viz.colNameMap(); 
+    xAData = viz.getColumn('A-xA')
+    AData = viz.getColumn('Ast')
+    filtered_xA = viz.remAlphaBs(xAData);
+    filtered_A = viz.remAlphaBs(AData)
+
+    viz.visualise(filtered_xA, filtered_A);
+    # print(titles)
 
 if __name__ == "__main__":
     main()
